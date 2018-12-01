7381d87d35703c810054426becd0accac232a5c3
ekamon.m <ekamon.m@gmail.com>
2018-12-02 02:03:51 +0700

    commit all function

--- bankapi.go
+++ bankapi.go
@@ -23,6 +23,9 @@ type BankAccount struct {
 	AcctNo 		  int `json:"acct_no"`
 	AcctName      string `json:"acct_name"`
 	Balance       int `json:"balance"`
+	Amount       int `json:"amount"`
+	From		 int `json:"from"`
+	To			 int `json:"to"`
 }
 
 type UserService interface {
@@ -36,7 +39,11 @@ type UserService interface {
 
 type AccountService interface {
 	InsertAcct(userid int,bacct *BankAccount) error
-	
+	GetAcctByID(userid int) ([]BankAccount, error)
+	DeleteAcctByID(id int) error
+	UpdateBalance(id int,balance int, acct *BankAccount) (*BankAccount, error)
+	GetAcctByAcctID(id int) (*BankAccount, error)
+	GetAcctByAcctNo(id int) (*BankAccount, error)
 }
 
 type UserServiceImp struct {
@@ -203,22 +210,171 @@ func (s *AccountServiceImp) InsertAcct(userid int,acct *BankAccount) error {
 	return nil
 }
 
+func (s *Server) GetAcctByID(c *gin.Context) {
+	userid, _ := strconv.Atoi(c.Param("id"))
+	accts, err := s.acctService.GetAcctByID(userid)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"object":  "error",
+			"message": fmt.Sprintf("db: query error: %s", err),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, accts)
+}
+func (s *AccountServiceImp) GetAcctByID(userid int) ([]BankAccount, error) {
+	stmt := "SELECT acct_no,acct_name FROM accounts WHERE user_id = ? ORDER BY acct_no DESC"
+	rows, err := s.db.Query(stmt,userid)
+	if err != nil {
+		return nil, err
+	}
+	bankAccts := []BankAccount{} // set empty slice without nil
+	for rows.Next() {
+		var acct BankAccount
+		err := rows.Scan(&acct.AcctNo, &acct.AcctName)
+		if err != nil {
+			return nil, err
+		}
+		bankAccts = append(bankAccts, acct)
+	}
+	return bankAccts, nil
+}
+func (s *Server) DeleteAcctByID(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	if err := s.acctService.DeleteAcctByID(id); err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+}
+
+func (s *AccountServiceImp) DeleteAcctByID(id int) error {
+	stmt := "DELETE FROM accounts WHERE id = ?"
+	_, err := s.db.Exec(stmt, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Server) WithDraw(c *gin.Context) {
+	var acct BankAccount
+	err := c.ShouldBindJSON(&acct)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"object":  "error",
+			"message": fmt.Sprintf("json: wrong params: %s", err),
+		})
+		return
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	amount := acct.Amount
+	getacct, err := s.acctService.GetAcctByAcctID(id)
+	balance := getacct.Balance-amount
+	acctupdated, err := s.acctService.UpdateBalance(id,balance,&acct)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, acctupdated)
+}
+
+func (s *Server) Deposit(c *gin.Context) {
+	var acct BankAccount
+	err := c.ShouldBindJSON(&acct)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"object":  "error",
+			"message": fmt.Sprintf("json: wrong params: %s", err),
+		})
+		return
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	amount := acct.Amount
+	getacct, err := s.acctService.GetAcctByAcctID(id)
+	balance := getacct.Balance+amount
+	acctupdated, err := s.acctService.UpdateBalance(id,balance,&acct)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, acctupdated)
+}
+
+func (s *Server) Transfers(c *gin.Context) {
+	var acct BankAccount
+	err := c.ShouldBindJSON(&acct)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"object":  "error",
+			"message": fmt.Sprintf("json: wrong params: %s", err),
+		})
+		return
+	}
+	amount := acct.Amount
+	getacctFrom, err := s.acctService.GetAcctByAcctNo(acct.From)
+	fromBalance := getacctFrom.Balance-amount
+	from, err := s.acctService.UpdateBalance(getacctFrom.ID,fromBalance,&acct)
+
+	getacctTo, err := s.acctService.GetAcctByAcctNo(acct.To)
+	toBalance := getacctTo.Balance+amount
+	_, err = s.acctService.UpdateBalance(getacctTo.ID,toBalance,&acct)
+	
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, from)
+}
+
+func (s *AccountServiceImp) UpdateBalance(id int,balance int,acct *BankAccount) (*BankAccount, error) {
+
+	stmt := "UPDATE accounts SET balance = ? WHERE id = ?"
+	_, err := s.db.Exec(stmt,balance,id)
+	if err != nil {
+		return nil, err
+	}
+	return s.GetAcctByAcctID(id)
+}
+
+func (s *AccountServiceImp) GetAcctByAcctID(id int) (*BankAccount, error) {
+	stmt := "SELECT id, acct_no, acct_name, balance FROM accounts WHERE id = ?"
+	row := s.db.QueryRow(stmt, id)
+	var acct BankAccount
+	err := row.Scan(&acct.ID,&acct.AcctNo, &acct.AcctName, &acct.Balance)
+	if err != nil {
+		return nil, err
+	}
+	return &acct, nil
+}
+
+func (s *AccountServiceImp) GetAcctByAcctNo(acctNo int) (*BankAccount, error) {
+	stmt := "SELECT id, acct_no, acct_name, balance FROM accounts WHERE acct_no = ?"
+	row := s.db.QueryRow(stmt, acctNo)
+	var acct BankAccount
+	err := row.Scan(&acct.ID,&acct.AcctNo, &acct.AcctName, &acct.Balance)
+	if err != nil {
+		return nil, err
+	}
+	return &acct, nil
+}
 
 func setupRoute(s *Server) *gin.Engine {
 	r := gin.Default()
-	users := r.Group("/users")
-
-	users.Use(gin.BasicAuth(gin.Accounts{
-		"admin": "1234",
+	bau := r.Group("/",gin.BasicAuth(gin.Accounts{
+		"admin":"1234",
 	}))
+	bau.GET("/users", s.All)
+	bau.POST("/users", s.Create)
+	bau.GET("/users/:id", s.GetByID)
+	bau.PUT("users/:id", s.Update)
+	bau.DELETE("users/:id", s.DeleteByID)
 
-	users.GET("/", s.All)
-	users.POST("/", s.Create)
-	users.GET("/:id", s.GetByID)
-	users.PUT("/:id", s.Update)
-	users.DELETE("/:id", s.DeleteByID)
-
-	users.POST("/:id/bankAccounts", s.InsertAcct)
+	bau.POST("/users/:id/bankAccounts", s.InsertAcct)
+	bau.GET("/users/:id/bankAccounts", s.GetAcctByID)
+	bau.DELETE("/bankAccounts/:id", s.DeleteAcctByID)
+	bau.PUT("/bankAccounts/:id/withdraw",s.WithDraw)
+	bau.PUT("/bankAccounts/:id/deposit",s.Deposit)
+	bau.POST("/transfers",s.Transfers)
 	return r
 }
 func main() {
